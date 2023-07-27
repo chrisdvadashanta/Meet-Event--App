7ef7cfaf73cb34f8e2994feff7106e77036a248c
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-07-27 12:26:48 +0200

    setting up the first Test with Jest for checking the DOM

---
+++ src/__test__/App.test.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import App from '../App.js';
+import React from 'react';
+
+
+describe('<App /> component', () => {
+    test('renders list of events', () => {
+        const AppDOM = render(<App />).container.firstChild;
+        expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+      });
+});
