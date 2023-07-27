61819768d342e4d62fbe9050d9e9b34828ebd002
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-07-27 12:27:09 +0200

    setting up first Test DOM changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import React from 'react';
+import EventList from './components/EventList';
+
 
 const App = () => {
   return (
     <div className="App">
+        <EventList />
     </div>
   );
  }
